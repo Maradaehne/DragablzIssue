76e4fc14ef654319fd7b3b864a7136fb91a1b04b
Maradaehne <Maradaehne@MOLA>
2017-02-07 02:23:04 +0100

    Projektdateien hinzufügen.

---
+++ DragablzIssue/TabClient/MyInterTabClient.cs
@@ -0,0 +1,25 @@
+﻿using Dragablz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DragablzIssue.TabClient
+{
+    class MyInterTabClient : IInterTabClient
+    {
+        public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            var shell = new MyInterTabWindow() { DataContext = new MyInterTabViewModel(interTabClient, partition)};
+
+            return new NewTabHost<Window>(shell, shell.InitialTabablzControl);
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            return TabEmptiedResponse.CloseWindowOrLayoutBranch;
+        }
+    }
+}
